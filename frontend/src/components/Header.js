c428f37ee7d0c651186530e64aba06f2a45d17ca
AbhyasKanaujia <44172451+AbhyasKanaujia@users.noreply.github.com>
2022-10-04 01:00:17 +0530

    react bootstrap navbar collapse on click

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 import Navbar from "react-bootstrap/Navbar";
 import Container from "react-bootstrap/Container";
@@ -7,36 +7,49 @@ import NavDropdown from "react-bootstrap/NavDropdown";
 import { Link } from "react-router-dom";
 
 const Header = ({ user }) => {
+  const [expanded, setExpanded] = useState(false);
+
   return (
-    <Navbar bg="light" expand="sm">
+    <Navbar expanded={expanded} bg="light" expand="sm">
       <Container>
         <Navbar.Brand as={Link} to="/">
           Tiffin App
         </Navbar.Brand>
-        <Navbar.Toggle aria-controls="navbar" />
+        <Navbar.Toggle
+          onClick={() => setExpanded(expanded ? false : "expanded")}
+          aria-controls="navbar"
+        />
         <Navbar.Collapse id="navbar">
           <Nav className="me-auto">
-            <Nav.Link as={Link} to="/">
+            <Nav.Link as={Link} to="/" onClick={() => setExpanded(false)}>
               Home
             </Nav.Link>
-            <Nav.Link as={Link} to="/search">
+            <Nav.Link as={Link} to="/search" onClick={() => setExpanded(false)}>
               Search
             </Nav.Link>
           </Nav>
           <Nav className="ms-auto">
-            <Nav.Link as={Link} to="/login">
+            <Nav.Link as={Link} to="/login" onClick={() => setExpanded(false)}>
               Login
             </Nav.Link>
-            <Nav.Link as={Link} to="/signup">
+            <Nav.Link as={Link} to="/signup" onClick={() => setExpanded(false)}>
               Signup
             </Nav.Link>
           </Nav>
           <Nav>
             <NavDropdown title={`Abhyas Kumar Kanaujia`}>
-              <NavDropdown.Item as={Link} to="/profile">
+              <NavDropdown.Item
+                as={Link}
+                to="/profile"
+                onClick={() => setExpanded(false)}
+              >
                 Profile
               </NavDropdown.Item>
-              <NavDropdown.Item as={Link} to="/logout">
+              <NavDropdown.Item
+                as={Link}
+                to="/logout"
+                onClick={() => setExpanded(false)}
+              >
                 Logout
               </NavDropdown.Item>
             </NavDropdown>
